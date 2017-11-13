e213bcca7793a00e089f58e353c27a312c226f13
FerranPares <ferran.pares.pont@gmail.com>
2017-11-13 12:01:43 +0100

    Code commented and MIT67 URL added into the Readme file

--- _codes/3.EMB/Image_Emb/visualization.py
+++ _codes/3.EMB/Image_Emb/visualization.py
@@ -2,14 +2,18 @@ import numpy as np
 from sklearn.manifold import TSNE
 from matplotlib import pyplot as plt
 
+# Load dataset embeddings and labels.
 obj = np.load('mit67_embeddings.npz')
 labels = obj['labels']
 embeddings = obj['embeddings']
 
+# Set up the subset of labels to visualize.
 n_labels = 5
 _uniq = np.unique(labels)[:n_labels]
 print 'The {n} labels selected to visualize:'.format(n=n_labels), _uniq
 
+# Create the embeddings matrix (and its corresponding labels list) 
+# containing instances belonging to labels in the previous subset.
 subsets_emb = np.zeros((0,4096))
 lab_range = {}
 offset = 0
@@ -21,6 +25,7 @@ for idx, _lab in enumerate(_uniq):
 print 'subsets_emb shape:', subsets_emb.shape
 vis_matrix = TSNE(n_components=2).fit_transform(subsets_emb)
 
+# Using matplotlib to create the plot and show it!
 cmap = plt.get_cmap('gist_rainbow')
 colors = [cmap(i) for i in np.linspace(0, 1, n_labels)]
 plt.figure()
